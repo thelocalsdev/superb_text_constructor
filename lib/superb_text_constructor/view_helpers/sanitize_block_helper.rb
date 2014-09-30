3944e19bf02368c6fc4a5f8bc1e6ffc1b405b225
Alexander Borovykh <immaculate.pine@gmail.com>
2014-09-30 14:16:02 +0400

    Sanitize and paragraph now are 2 separate methods

--- lib/superb_text_constructor/view_helpers/sanitize_block_helper.rb
+++ lib/superb_text_constructor/view_helpers/sanitize_block_helper.rb
@@ -2,13 +2,28 @@ module SuperbTextConstructor
   module ViewHelpers
     module SanitizeBlockHelper
 
+      # Removes forbidden tags from text
+      # @param text [String] original text
+      # @return [ActiveSupport::SafeBuffer] HTML safe text with permitted only tags
+      def sanitize_block(text)
+        sanitize(text, tags: %w(a b i img br), attributes: %w(id class style src href target))
+      end
+
+
       # Adds HTML markup to plain text with paragraphs separated by new lines and removes forbidden tags.
       # @param text [String] original text
       # @return [ActiveSupport::SafeBuffer] HTML safe text splitted by <p> tags
-      def sanitize_block(text)
-        text = sanitize(text, tags: %w(a b i img), attributes: %w(id class style src href target))
-        paragraphs = text.split("\r\n\r\n").map(&:strip).select(&:present?)
-        paragraphs.map{|p| "<p>#{p}</p>"}.join("\r\n").html_safe
+      def sanitize_and_paragraph_block(text)
+        sanitize_and_split_block(text, 'p')
+      end
+
+      # Adds HTML markup to plain text with paragraphs separated by new lines and removes forbidden tags.
+      # @param text [String] original text
+      # @param tag [Symbol] wrapper tag name. E.g. :p, :li
+      # @return [ActiveSupport::SafeBuffer] HTML safe text splitted by selected tags
+      def sanitize_and_split_block(text, tag)
+        paragraphs = sanitize_block(text).split("\r\n\r\n").map(&:strip).select(&:present?)
+        paragraphs.map { |p| content_tag tag do p.html_safe end }.join("\r\n").html_safe
       end
 
     end
